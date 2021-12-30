e5bc279dc00f9b88c3c88d369ef705865ae508d4
Joe Moder <joe.moder.github@gmail.com>
2021-12-30 00:27:53 -0500

    Add some basic sim for use without I/O pins Attempt to add camera stream (working on stream conversion details)

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 This project is for a RC car controlled by a Raspberry Pi.  The Pi controls the car by connecting GPIOs into the buttons on the RC remote.  The Pi hosts a webserver which provides buttons for the user to press to drive the car.
 
+# Setup
+* Install node on the raspberry pi 
+** See NVM page to install NVM
+** `nvm install v16.13.1`
+** `npm install`
+** `node webserver.js`
+
 
 ## This project uses FontAwesome see below!
 
--- webserver.js
+++ webserver.js
@@ -9,12 +9,18 @@ var fwd_gpio = new Gpio(4,'high');
 var left_gpio = new Gpio(27,'high');
 var right_gpio = new Gpio(22,'high');
 
+var rev_sim_gpio = new Gpio(15,'in', 'both');
+var fwd_sim_gpio = new Gpio(14,'in', 'both');
+var left_sim_gpio = new Gpio(18,'in', 'both');
+var right_sim_gpio = new Gpio(23,'in', 'both');
+
+console.log("Starting WiPi Car!!!")
 http.listen(8080); //listen to port 8080
 
 function toggle_input(name, gpio, val) {
     var inv_val = val ^ 1;
     if(inv_val != gpio.readSync()) {
-        //console.log(name + ": " + (inv_val == 0 ? "on" : "off"));
+        console.log(name + ": " + (inv_val == 0 ? "on" : "off"));
         gpio.writeSync(inv_val);
     }
 }
@@ -72,20 +78,47 @@ function handler (request, response) { //create server
     });
 }
 
+function send_sim_gpio(socket, socket_str, err, value) {
+    if(err) {
+        console.error('Error:', err);
+        return;
+    }
+    console.log('Emiting: ' + socket_str + ': ' + value);
+    socket.emit(socket_str, value);
+}
+
 io.sockets.on('connection', function (socket) {
+    console.log("Socket connected!");
     socket.on('fwd', function(data) {
         toggle_input("fwd", fwd_gpio, data);
+        send_sim_gpio(socket, 'fwd_sim', '', data);
     });
     socket.on('rev', function(data) {
         toggle_input("rev", rev_gpio, data);
+        send_sim_gpio(socket, 'rev_sim', '', data);
     });
     socket.on('left', function(data) {
         toggle_input("left", left_gpio, data);
+        send_sim_gpio(socket, 'left_sim', '', data);
     });
     socket.on('right', function(data) {
         toggle_input("right", right_gpio, data);
+        send_sim_gpio(socket, 'right_sim', '', data);
     });
+
+
+    rev_sim_gpio.watch(function (err, value) { send_sim_gpio(socket, 'rev_sim', err, value); });
+    fwd_sim_gpio.watch(function (err, value) { send_sim_gpio(socket, 'fwd_sim', err, value); });
+    left_sim_gpio.watch(function (err, value) { send_sim_gpio(socket, 'left_sim', err, value); });
+    right_sim_gpio.watch(function (err, value) { send_sim_gpio(socket, 'right_sim', err, value); });
 });
+
+function unexportOnClose() { //function to run when exiting program
+  rev_sim_gpio.unexport(); // Unexport Button GPIO to free resources
+  fwd_sim_gpio.unexport(); // Unexport Button GPIO to free resources
+  left_sim_gpio.unexport(); // Unexport Button GPIO to free resources
+  right_sim_gpio.unexport(); // Unexport Button GPIO to free resources
+};
 
 function closeGpio(gpio) {
     gpio.writeSync(1);
@@ -94,10 +127,16 @@ function closeGpio(gpio) {
 }
 
 process.on('SIGINT', function() {
+    //Close driving GPIOs
     closeGpio(rev_gpio);
     closeGpio(fwd_gpio);
     closeGpio(left_gpio);
     closeGpio(right_gpio);
+    //Close sim GPIOs
+    closeGpio(rev_sim_gpio);
+    closeGpio(fwd_sim_gpio);
+    closeGpio(left_sim_gpio);
+    closeGpio(right_sim_gpio);
     process.exit();
 });
 
